30277d7b7366dcd0cc44bdebeb24e05eddf797d8
Sidhbtu <50226798+Sidhbtu@users.noreply.github.com>
2021-01-06 21:23:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ let csvStream = csv.fromPath(".\\csv\\5m Sales Records.csv", { headers: true })
             let Total_Cost = record.Total_Cost;
             let Total_Profit = record.Total_Profit;
 //make order_id as primary key
-            pool.query("INSERT INTO FL_insurance_sample(Order_ID, Region, Country, Item_Type, Sales_Channel, Order_Priority, Order_Date,Ship_Date,Units_Sold,Unit_Price,Unit_Cost,Total_Revenue,Total_Cost,Total_Profit) \
+            pool.query("INSERT INTO 5m_Sales_Records(Order_ID, Region, Country, Item_Type, Sales_Channel, Order_Priority, Order_Date,Ship_Date,Units_Sold,Unit_Price,Unit_Cost,Total_Revenue,Total_Cost,Total_Profit) \
             VALUES($1, $2, $3, $4, $5, $6, $7,$8, $9, $10, $11, $12, $13, $14)", [Order_ID, Region, Country, Item_Type, Sales_Channel, Order_Priority, Order_Date,Ship_Date,Units_Sold,Unit_Price,Unit_Cost,Total_Revenue,Total_Cost,Total_Profit], function(err){
                 if(err)
                 {
